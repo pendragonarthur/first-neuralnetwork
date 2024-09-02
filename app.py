3767496e6d77c3519188d5749143fcc0a40ddf7e
arthur <devarthurquarxma@outlook.com>
2024-09-02 20:52:15 -0300

    fix/feat: fixed function errors, added missing functions

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import tensorflow as tf
 train_images = train_images / 255.0
 test_images = test_images / 255.0 
 
+##############FUNCTIONS##############
+
 def relu(x):
     return np.maximum(0, x)
 
@@ -21,30 +23,44 @@ def forwardpropagation(inputs, layers):
         
     return current_output
 
+def he_initialization(shape):
+    return np.random.randn(*shape) * np.sqrt(2.0/shape[1])
+
+def cross_loss(y_pred, y_true):
+    return -np.sum(y_true * np.log(y_pred + 1e-8))
+
+def softmax_grad(y_pred, y_true):
+    return y_pred - y_true
+
+def update_weights(weights, biases, grad_weights, grad_biases, learning_rate):
+    weights -= learning_rate * grad_weights
+    biases -= learning_rate * grad_biases
+    return weights, biases
+
+##############LAYERS##############
+
+
 def input_layer(num_inputs, num_neurons):
-    input_size = num_inputs
-    weights = [[round(np.random.uniform(-1.0, 1.0), 2) for _ in range(input_size)] for _ in range(num_neurons)]
-    bias = [round(np.random.uniform(-1.0, 1.0), 2) for _ in range(num_neurons)]
+    weights = he_initialization(num_inputs, num_neurons)
+    bias = np.zeros(num_neurons)
     return {"weights": weights, "bias": bias}
     
 def hidden_layers(num_layers, num_neurons_per_layer, num_inputs):
     layers = []
-    input_size = num_inputs
 
     for _ in range(num_layers):
-        weights = np.random.uniform(-1.0, 1.0, (num_neurons_per_layer, input_size))
-        bias = np.random.uniform(-1.0, 1.0, (num_neurons_per_layer))
+        weights = he_initialization(num_inputs, num_neurons_per_layer)
+        bias = np.zeros(num_neurons_per_layer)
         layers.append({"weights": weights, "bias": bias})
         input_size = num_neurons_per_layer
 
     return layers
     
 def output_layer(num_inputs, hidden_layer_output, num_outputs=10):
-    input_size = num_inputs
-    weights = np.random.uniform(-1.0, 1.0, (num_outputs, input_size))
-    bias = np.random.uniform(-1.0, 1.0, num_outputs)
-    weighted_sum = np.dot(weights, hidden_layer_output) + bias
-    output = softmax(weighted_sum)
+    weights = he_initialization(num_inputs, num_outputs)
+    bias = np.zeros(num_outputs)
+    logits = np.dot(weights, hidden_layer_output) + bias
+    output = softmax(logits)
 
     return output
 
@@ -55,10 +71,10 @@ num_outputs = 10
 
 input_layer_config = input_layer(num_inputs, num_neurons_per_layer)
 hidden_layers_config = hidden_layers(num_hidden_layers, num_neurons_per_layer, num_inputs)
-output_layer_config = {"weights": np.random.uniform(-1.0, 1.0, (num_outputs, num_neurons_per_layer)),
-                       "bias": np.random.uniform(-1.0, 1.0, num_outputs)}
+output_layer_config = {"weights": he_initialization((num_outputs, num_neurons_per_layer)),
+                       "bias": np.zeros(num_outputs)}
 
-inputs = np.random.rand(num_inputs)
+inputs = train_images[0].flatten()
 hidden_outputs = forwardpropagation(inputs, hidden_layers_config)
 final_output = output_layer(num_neurons_per_layer, hidden_outputs, num_outputs)
 print("Final Output:", final_output)
