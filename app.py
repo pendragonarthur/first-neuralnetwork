b99d91dab7f8f436fbe313aa047eed9c7ae9686f
arthur <devarthurquarxma@outlook.com>
2024-09-10 14:11:11 -0300

    feat: added training loop

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import numpy as np
 import tensorflow as tf
 
-(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
-train_images = train_images / 255.0
-test_images = test_images / 255.0 
+
 
 ##############FUNCTIONS##############
 
@@ -52,7 +50,6 @@ def hidden_layers(num_layers, num_neurons_per_layer, num_inputs):
         weights = he_initialization(num_inputs, num_neurons_per_layer)
         bias = np.zeros(num_neurons_per_layer)
         layers.append({"weights": weights, "bias": bias})
-        input_size = num_neurons_per_layer
 
     return layers
     
@@ -74,7 +71,37 @@ hidden_layers_config = hidden_layers(num_hidden_layers, num_neurons_per_layer, n
 output_layer_config = {"weights": he_initialization((num_outputs, num_neurons_per_layer)),
                        "bias": np.zeros(num_outputs)}
 
-inputs = train_images[0].flatten()
-hidden_outputs = forwardpropagation(inputs, hidden_layers_config)
-final_output = output_layer(num_neurons_per_layer, hidden_outputs, num_outputs)
-print("Final Output:", final_output)
+##############TRAINING AND VALIDATION##############
+
+(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+train_images = train_images / 255.0
+test_images = test_images / 255.0 
+
+batch_size = 32
+epoch_size = 10
+
+for epochs in range(epoch_size):
+    loss = 0
+
+    for i in range(0, len(train_images), batch_size) # Iterando sobre o dataset começando do 0, até len(train_images), em grupos de batch_size (32)
+    batch_images = train_images[i:i+batch_size].reshape(batch_size, -1)
+    batch_labels = train_labels[i:i+batch_size]
+
+    hidden_outputs = forwardpropagation(batch_images.T, hidden_layers_config) # Forward propagation nas camadas ocultas
+    
+    logits = np.dot(output_layer_config["weights"], hidden_outputs) + output_layer_config["bias"] # Forward propagation na camada de output
+    predictions = softmax(logits)
+
+    batch_loss = cross_loss(predictions, batch_labels) # Calculo de perda do batch
+    loss += batch_loss
+
+    grad_output = softmax_grad(predictions, batch_labels) # Backpropagation do gradiente de output
+    grad_weights_output = np.dot(grad_output, hidden_outputs.T)
+    grad_bias_output = np.sum(grad_output, axis=1, keepdims=True) # O bias é simplesmente a soma dos outputs do gradiente, axis=1 indica que estamos somando ao longo do eixo dos exemplos do batch, keepdims=True mantém a dimensão original do array 2D 
+
+    # Atualização dos pesos e bias
+    output_layer_config["weights"], output_layer_config["bias"] = update_weights(
+        output_layer_config["weights"], output_layer_config["bias"], grad_weights_output, grad_bias_output, learning_rate=0.01
+    )
+
+    
